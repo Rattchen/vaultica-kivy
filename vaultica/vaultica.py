cbf57a2f6ac26b9b8af79a7f61566a5a2ae0a609
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-03-16 04:51:20 +0100

    Add a button to update the Label's text

--- vaultica/vaultica.py
+++ vaultica/vaultica.py
@@ -1,14 +1,23 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
 
 
 class Vaultica(Widget):
-    pass
+    title_text = StringProperty("Initial entry")
 
+    def fetch_data(self):
+        return "Mocked data"
+
+    def update_label(self):
+        self.title_text = self.fetch_data()
 
 class VaulticaApp(App):
+
     def build(self):
         return Vaultica()
+
+    
 
 
 if __name__ == '__main__':
